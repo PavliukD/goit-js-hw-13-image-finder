078b365841c43f4c535c935f18b4c9c42811bdeb
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-09-24 14:47:55 +0300

    начата работа над...

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import refs from './js/refs'
 
-const { BASE_URL, apiKey, perPage } = refs
+const { input, galley } = refs
 
-async function apiServise(baseUrl, key, perPage){
-    let url = `${baseUrl}?key=${key}&image_type=photo&orientation=horizontal&per_page=${perPage}&page=1&q=flower`
-    let r = await fetch(`${url}`)
-    let d = await r.json()
-    console.log(d.hits)
-    // return d
+input.addEventListener('submit', onInput)
+
+
+function onInput(e){
+    e.preventDefault()
+    console.log(e.target.elements.search.value)
 }
-
-
-apiServise(BASE_URL, apiKey, perPage)
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,15 +3,17 @@ import refs from './refs'
 const { BASE_URL, apiKey, perPage } = refs
 
 export default class APIServise(){
- constructor(){
+ constructor(input, page){
      this.baseUrl = BASE_URL
      this.key = apiKey
      this.pPage = perPage
+     this.input = input
+     this.page = page
 
  }
 
  async getFetch(){
-    let url = `${this.baseUrl}?key=${this.key}&image_type=photo&orientation=horizontal&per_page=${this.pPage}&page=1&q=flower`
+    let url = `${this.baseUrl}?key=${this.key}&image_type=photo&orientation=horizontal&per_page=${this.pPage}&page=${this.page}&q=${this.input}`
     let r = await fetch(`${url}`)
     let d = await r.json()
     console.log(d.hits)
