c43097686df004ab6666950b59243f199ec0635f
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-09-27 14:26:56 +0300

    добавлена минимальная стилизация

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import apiService from './js/apiService'
 import imageCards from './templates/image-card.hbs'
 
 const { input, gallery } = refs
+let page = 1
 
 input.addEventListener('submit', onInput)
 
@@ -10,7 +11,7 @@ input.addEventListener('submit', onInput)
 async function onInput(e){
     e.preventDefault()
     let query = e.target.elements[0].value
-    let data = await apiService(query, 1)
+    let data = await apiService(query, page)
     renderElements(data)
 }
 
