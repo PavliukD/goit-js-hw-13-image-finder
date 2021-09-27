d5b0fcf1d99df576a23dca9f8d8b88c3c2f28951
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-09-27 14:46:44 +0300

    добавлена перерисовка галереи при изменении запроса

--- src/index.js
+++ src/index.js
@@ -4,15 +4,24 @@ import imageCards from './templates/image-card.hbs'
 
 const { input, gallery } = refs
 let page = 1
+let query = ''
 
 input.addEventListener('submit', onInput)
 
 
 async function onInput(e){
     e.preventDefault()
-    let query = e.target.elements[0].value
-    let data = await apiService(query, page)
-    renderElements(data)
+    if (query !== e.target.elements[0].value){
+        gallery.innerHTML = ''
+        page = 1
+        query = e.target.elements[0].value
+        let data = await apiService(query, page)
+        renderElements(data)
+        return
+    }
+    // query = e.target.elements[0].value
+    // let data = await apiService(query, page)
+    // renderElements(data)
 }
 
 function renderElements(data){
